4baa29c68ee093f5ac8614dbae9341bb6b2f792e
pug <115802867+PugPostsCode@users.noreply.github.com>
2023-04-26 15:27:03 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
 function notForSale() {
     alert("This is a ficticious website and these products are not for sale :(");
 }
+
+function formSubmitted() {
+    alert("We will not get back to you in 5-12 business days, as this is a ficticious website.");
+}
